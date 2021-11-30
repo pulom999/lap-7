31287ef79b6a2ba3929af7184367986f6646850a
pulom999 <pulom999@gmail.com>
2021-11-30 16:22:05 +0700

    Lab7_2

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,10 +4,24 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
-		P[i] = 2*i+1;
-		i++;
+	int i = 0, P[5000], k, result=0,x=0;
+	while(x < 5000){
+		P[x] = 2*x+1;
+		x++;
+	}
+	cout << "Please input k: ";
+	cin>>k;
+	if(k>0){
+		while(i<5000){
+			if((P[i]%k)==0){
+				result = result-P[i];
+			}else{
+				result = result+P[i];
+			}i++;
+		}
+		cout <<"Result = "<<result;
+	}else{
+		cout<<"Invalid input!!!";
 	}
 	
 	return 0;
